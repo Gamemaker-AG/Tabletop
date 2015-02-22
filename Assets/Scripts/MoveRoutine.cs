a2288077d5f355a39ae0b0edb2c22835f585c9d2
hauke96 <mail@hauke-stieler.de>
2015-02-22 22:18:28 +0100

    Added walking routine for the characters

---
+++ Assets/Scripts/MoveRoutine.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRoutine : GameMode 
+{
+	Player _player;
+
+	public MoveRoutine(Player player)
+	{
+        _player = player;
+	}
+	public void OnGUI () 
+	{
+	    
+	}
+	public void Update () 
+	{
+        _player.moveCharacter();
+	}
+}
